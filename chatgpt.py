4aec46665f9b44fd2ecbf970eebc0fdd533f536a
naskar-akash <naskar.akash1947@gmail.com>
2026-03-18 11:24:32 +0530

    setting up chatgpt message prompt

--- chatgpt.py
+++ chatgpt.py
@@ -12,7 +12,24 @@ def ask_chatgpt(prompt):
         response = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "You are Jarvis, a virtual assistance who is enable to provide short but important responoses within three or four lines"},
+                {"role": "system", "content": 
+                """You are Jarvis, a smart virtual voice assistance 
+                Behavior:
+                    - Keep responses under 3 lines.
+                    - Prioritize actions over explanations.
+                    - If a command is given, confirm execution briefly.
+                    - If it's a question, answer concisely.
+                    - If unsure, ask a short follow-up question.
+                    - Do not include unnecessary details.
+
+                Style:
+                    - Professional but friendly.
+                    - Confident and assistant-like.
+
+                Special:
+                    - Do not say "As an AI model".
+                    - Do not give long paragraphs unless explicitly asked.
+                """},
                 {"role": "user", "content": prompt}
             ]
         )
